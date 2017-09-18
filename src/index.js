866e11ab110023a10e7010e11e93b5d5246eadec
Hui <252700589@qq.com>
2017-09-18 15:07:36 +0800

    完成基本骨架

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Welcome from './Welcome'
+import App from './App';
 ReactDOM.render(
   <Welcome name="frank"/>,
   document.getElementById('root2')
