406baccb30dd6ccec286b45f72dd817ba0fb4f8a
Hui <252700589@qq.com>
2017-09-18 14:23:38 +0800

    展示钩子

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import Welcome from './Welcome'
-import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <Welcome name="frank"/>,
   document.getElementById('root2')
